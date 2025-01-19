6de09368df62bf499265cc5d55e53c529f8d8a29
DESKTOP-T5B94PA\swanb <swanbaek@naver.com>
2025-01-19 19:01:35 +0900

    website등 기타 예제 추가

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    bracketSpacing: false,
+    jsxBracketSameLine: true,
+    printWidth: 120,
+    tabWidth: 4,
+    singleQuote: true,
+    trailingComma: 'es5',
+    semi: false,
+    arrowParens: 'avoid'
+}
